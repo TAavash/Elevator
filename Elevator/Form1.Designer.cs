fa4c0afb82a16e53b7048a1854d525fc2075fd2c
Aavash Tamang <aavashlopchan@gmail.com>
2024-10-22 15:05:47 +0545

    Add project files.

---
+++ Elevator/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Elevator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            closeBtn = new Button();
+            openBtn = new Button();
+            pictureBox2 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            rightDoor_G = new PictureBox();
+            leftDoor_G = new PictureBox();
+            leftDoor_T = new PictureBox();
+            rightDoor_T = new PictureBox();
+            timer2 = new System.Windows.Forms.Timer(components);
+            dataGridViewLogs = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rightDoor_G).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)leftDoor_G).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)leftDoor_T).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rightDoor_T).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewLogs).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.liftopen;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(63, 275);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(158, 175);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = Properties.Resources.Screenshot_2024_10_18_074020;
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(314, 109);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 66);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = Properties.Resources.Screenshot_2024_10_18_080230;
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.Location = new Point(314, 181);
+            button2.Name = "button2";
+            button2.Size = new Size(78, 64);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // closeBtn
+            // 
+            closeBtn.BackgroundImage = Properties.Resources.Screenshot_2024_10_18_074100;
+            closeBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            closeBtn.Location = new Point(270, 265);
+            closeBtn.Name = "closeBtn";
+            closeBtn.Size = new Size(75, 69);
+            closeBtn.TabIndex = 3;
+            closeBtn.UseVisualStyleBackColor = true;
+            closeBtn.Click += button3_Click;
+            // 
+            // openBtn
+            // 
+            openBtn.BackgroundImage = Properties.Resources.Screenshot_2024_10_18_074114;
+            openBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            openBtn.Location = new Point(367, 265);
+            openBtn.Name = "openBtn";
+            openBtn.Size = new Size(79, 69);
+            openBtn.TabIndex = 4;
+            openBtn.UseVisualStyleBackColor = true;
+            openBtn.Click += button4_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = Properties.Resources.neon;
+            pictureBox2.Location = new Point(261, 48);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(195, 362);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // rightDoor_G
+            // 
+            rightDoor_G.BackgroundImage = Properties.Resources.liftpp;
+            rightDoor_G.Location = new Point(138, 275);
+            rightDoor_G.Name = "rightDoor_G";
+            rightDoor_G.Size = new Size(83, 175);
+            rightDoor_G.TabIndex = 6;
+            rightDoor_G.TabStop = false;
+            rightDoor_G.Click += pictureBox3_Click;
+            // 
+            // leftDoor_G
+            // 
+            leftDoor_G.BackgroundImage = Properties.Resources.leftdoor;
+            leftDoor_G.Location = new Point(63, 275);
+            leftDoor_G.Name = "leftDoor_G";
+            leftDoor_G.Size = new Size(76, 175);
+            leftDoor_G.TabIndex = 7;
+            leftDoor_G.TabStop = false;
+            leftDoor_G.Click += pictureBox4_Click;
+            // 
+            // leftDoor_T
+            // 
+            leftDoor_T.BackgroundImage = Properties.Resources.leftdoor;
+            leftDoor_T.Location = new Point(63, 0);
+            leftDoor_T.Name = "leftDoor_T";
+            leftDoor_T.Size = new Size(76, 175);
+            leftDoor_T.TabIndex = 8;
+            leftDoor_T.TabStop = false;
+            leftDoor_T.Click += pictureBox5_Click;
+            // 
+            // rightDoor_T
+            // 
+            rightDoor_T.BackgroundImage = Properties.Resources.liftpp;
+            rightDoor_T.Location = new Point(138, 0);
+            rightDoor_T.Name = "rightDoor_T";
+            rightDoor_T.Size = new Size(83, 175);
+            rightDoor_T.TabIndex = 9;
+            rightDoor_T.TabStop = false;
+            rightDoor_T.Click += pictureBox6_Click;
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
+            // dataGridViewLogs
+            // 
+            dataGridViewLogs.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewLogs.Location = new Point(479, 48);
+            dataGridViewLogs.Name = "dataGridViewLogs";
+            dataGridViewLogs.RowHeadersWidth = 51;
+            dataGridViewLogs.Size = new Size(300, 362);
+            dataGridViewLogs.TabIndex = 10;
+            dataGridViewLogs.CellContentClick += dataGridViewLogs_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridViewLogs);
+            Controls.Add(rightDoor_T);
+            Controls.Add(leftDoor_T);
+            Controls.Add(rightDoor_G);
+            Controls.Add(leftDoor_G);
+            Controls.Add(openBtn);
+            Controls.Add(closeBtn);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rightDoor_G).EndInit();
+            ((System.ComponentModel.ISupportInitialize)leftDoor_G).EndInit();
+            ((System.ComponentModel.ISupportInitialize)leftDoor_T).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rightDoor_T).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewLogs).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button closeBtn;
+        private Button openBtn;
+        private PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox rightDoor_G;
+        private PictureBox leftDoor_G;
+        private PictureBox leftDoor_T;
+        private PictureBox rightDoor_T;
+        private System.Windows.Forms.Timer timer2;
+        private DataGridView dataGridViewLogs;
+    }
+}
---
+++ Elevator/Form1.cs
@@ -0,0 +1,290 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Elevator
+{
+    public partial class Form1 : Form
+    {
+        int liftSpeed = 5;
+        bool isMovingUp = false;
+        bool isMovingDown = false;
+
+        bool isClosing = false;
+        bool isOpening = false;
+        int doorSpeed = 5;
+        int doorMaxOpenWidth;
+
+        bool isDoorOpen = false;
+
+        string connectString = @"Server = AAVASHT; Database = Lift; Trusted_Connection = True";
+
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            doorMaxOpenWidth = pictureBox1.Width / 2 ;
+            dataGridViewLogs.ColumnCount = 2;
+
+            dataGridViewLogs.Columns[0].Name = "Time";
+            dataGridViewLogs.Columns[1].Name = "Evets";
+
+            dt.Columns.Add("Time");
+            dt.Columns.Add("Events");
+
+        }
+        private void logEvents(string message)
+        {
+            string currentTime = DateTime.Now.ToString("hh:mm:ss");
+            dt.Rows.Add(currentTime, message);
+            dataGridViewLogs.Rows.Add(currentTime, message);
+
+            InsertLogsIntoDB(dt);
+        }
+
+        private void InsertLogsIntoDB(DataTable dt)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectString))
+                {
+                    string query = @"Insert into liftRecord(LogTIme, EventDescription) values(@Time, @Logs)";
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        adapter.InsertCommand = new SqlCommand(query, conn);
+                        adapter.InsertCommand.Parameters.Add("@Time", SqlDbType.DateTime, 0, "Time");
+                        adapter.InsertCommand.Parameters.Add("@Logs", SqlDbType.NVarChar, 255, "Events");
+
+                        conn.Open();
+
+                        adapter.Update(dt);
+                    }
+                }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show("Error while saving", ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (isDoorOpen) {
+                logEvents("Cannot move up, the damn door is open!");
+                return;
+            }
+
+            isMovingUp = true;
+            isMovingDown = false;
+            button1.BackColor = Color.LightGreen;
+            button2.BackColor = Color.White; // Reset the color of the down button
+            button1.Enabled = false;         // Disable the up button
+            button2.Enabled = false;         // Disable the down button during the movement
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isMovingUp)
+            {
+                if (pictureBox1.Top > 0)  // Ensure the elevator doesn’t go above the top
+                {
+                    pictureBox1.Top -= liftSpeed;
+                }
+                else
+                {
+                    pictureBox1.Top = 0;
+                    StopElevator();
+                    logEvents("Mathi pugyo");
+
+                    //button1.Enabled = true; // Enable the up button
+                    //button2.Enabled = true; // Enable the down button
+                }
+            }
+
+            if (isMovingDown)
+            {
+                if (pictureBox1.Bottom < this.ClientSize.Height) // Move down until the bottom is reached
+                {
+                    pictureBox1.Top += liftSpeed;
+                }
+                else
+                {
+                    pictureBox1.Top = this.ClientSize.Height - pictureBox1.Height; // Stop at the bottom
+                    StopElevator();
+                    logEvents("Rock Bottom");
+
+                    //button1.Enabled = true; // Enable the up button
+                    //button2.Enabled = true; // Enable the down button
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isDoorOpen)
+            {
+                logEvents("Cannot move down; the damn door is open!");
+                return;
+            }
+
+            isMovingDown = true;
+            isMovingUp = false;
+            button2.BackColor = Color.LightGreen;
+            button1.BackColor = Color.White; // Reset the color of the up button
+            button1.Enabled = false;         // Disable the up button during movement
+            button2.Enabled = false;         // Disable the down button during movement
+            timer1.Start();
+        }
+
+        private void StopElevator()
+        {
+            isMovingUp = false;
+            isMovingDown = false;
+            button1.BackColor = Color.White; // Reset the up button color
+            button2.BackColor = Color.White; // Reset the down button color
+            timer1.Stop();
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            isOpening = true;
+            isClosing = false;
+            timer2.Start();
+            closeBtn.Enabled = false;
+            logEvents("Lift khulyo bhitra jamm !!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isDoorOpen)
+            {
+                isOpening = false;
+                isClosing = true;
+                timer2.Start();
+                openBtn.Enabled = false;
+                logEvents("Lift banda don't move !!");
+            }
+            else
+            {
+                logEvents("Lift is already closed!");
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top != 0) {
+
+                if (isOpening)
+                {
+                    if (leftDoor_G.Left > 0)
+                    {
+                        leftDoor_G.Left -= doorSpeed;
+                        rightDoor_G.Left += doorSpeed;
+                    }
+                    else
+                    {
+                        timer2.Stop();
+                        closeBtn.Enabled = true;
+                        isDoorOpen = true;
+
+                    }
+                }
+
+                if (isClosing)
+                {
+                    if (leftDoor_G.Left < doorMaxOpenWidth - 20)
+                    {
+                        leftDoor_G.Left += doorSpeed;
+                        rightDoor_G.Left -= doorSpeed;
+                    }
+                    else
+                    {
+
+                        timer2.Stop();
+                        openBtn.Enabled = true;
+                        isDoorOpen = false;
+                    }
+
+                }
+            }
+
+
+            else
+            {
+                if (isOpening)
+                {
+                    if (leftDoor_T.Left > 0)
+                    {
+                        leftDoor_T.Left -= doorSpeed;
+                        rightDoor_T.Left += doorSpeed;
+                    }
+                    else
+                    {
+                        timer2.Stop();
+                        closeBtn.Enabled = true;
+                        isDoorOpen = true;
+
+                    }
+                }
+
+                if (isClosing)
+                {
+                    if (leftDoor_T.Left < doorMaxOpenWidth - 20)
+                    {
+                        leftDoor_T.Left += doorSpeed;
+                        rightDoor_T.Left -= doorSpeed;
+                    }
+                    else
+                    {
+
+                        timer2.Stop();
+                        openBtn.Enabled = true;
+                        isDoorOpen = false;
+                    }
+
+                }
+            }
+
+        }
+
+        private void dataGridViewLogs_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
